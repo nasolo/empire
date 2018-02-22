a002e1370a0d68c5ca8f2c836f3945e6f5fdbcdb
Randy Robinson <rrobinson@Randys-MacBook-Pro.local>
2018-02-21 23:02:37 -0600

    added installer page

--- public/src/views/installers/installers.js
+++ public/src/views/installers/installers.js
@@ -1,25 +1,117 @@
 import React, {Component} from 'react';
-import {Badge, Row, Col, Card, CardHeader, CardFooter, CardBody, Label, Input} from 'reactstrap';
+import {
+  Badge,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Table,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Button
+} from 'reactstrap';
 
 class Installers extends Component {
   render() {
     return (
       <div className="animated fadeIn">
         <Row>
-          <Col xs="12" sm="12" md="12" lg="12">
+          <Col xs="12" lg="12">
             <Card>
               <CardHeader>
-                List of Empire-Today Installers
+                
+              <Row>
+                <Col xs="12" lg="12" className="float-right">
+
+                <Button color="primary"><i className="fa fa-align-justify"></i> Installer</Button>{' '}
+                <Button color="primary">Primary</Button>{' '}
+
+              </Col>
+              </Row>
+                
               </CardHeader>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                <Table responsive>
+                  <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Date registered</th>
+                    <th>Role</th>
+                    <th>Status</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Samppa Nori</td>
+                    <td>2012/01/01</td>
+                    <td>Member</td>
+                    <td>
+                      <Badge color="success">Active</Badge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Estavan Lykos</td>
+                    <td>2012/02/01</td>
+                    <td>Staff</td>
+                    <td>
+                      <Badge color="danger">Banned</Badge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Chetan Mohamed</td>
+                    <td>2012/02/01</td>
+                    <td>Admin</td>
+                    <td>
+                      <Badge color="secondary">Inactive</Badge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Derick Maximinus</td>
+                    <td>2012/03/01</td>
+                    <td>Member</td>
+                    <td>
+                      <Badge color="warning">Pending</Badge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Friderik Dávid</td>
+                    <td>2012/01/21</td>
+                    <td>Staff</td>
+                    <td>
+                      <Badge color="success">Active</Badge>
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+                <Pagination>
+                  <PaginationItem>
+                    <PaginationLink previous href="#"></PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink href="#">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">4</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next href="#"></PaginationLink>
+                  </PaginationItem>
+                </Pagination>
               </CardBody>
             </Card>
           </Col>
+
         </Row>
       </div>
+
 
     )
   }
